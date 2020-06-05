3639c1d994ab1b242bc7d69a69ae274b04521b1f
Prashant Sail <psail4444@gmail.com>
2020-06-05 18:15:57 +0530

    comments and clean up

--- run_circleci_tests.py
+++ run_circleci_tests.py
@@ -11,6 +11,8 @@ from functools import reduce
 
 workflow = 'build_and_test'
 job = 'python_tests'
+
+circleci_config_file = '.circleci/config.yml'
 processed_file = 'processed.yml'
 processed_config = {}
 xformed_file = 'xformed.yml'
@@ -19,7 +21,7 @@ xformed_job_name = 'mms_xformed_job'
 blacklisted_steps = ['persist_to_workspace', 'attach_workspace', 'store_artifacts']
 
 # Create processed YAML using circleci cli's 'config process' commands
-subprocess.check_call('circleci config process .circleci/config.yml > {}'.format(processed_file), shell=True)
+subprocess.check_call('circleci config process {} > {}'.format(circleci_config_file, processed_file), shell=True)
 
 # Read the processed config
 with open(processed_file) as fstream:
@@ -57,14 +59,14 @@ def getJobSteps(result, job_name):
 merged_steps = reduce(getJobSteps, jobs_to_exec, [])
 
 # Create a new job, using the first job as a reference
-# This ensures config like executors, environments, etc are maintained as it is from the first job
+# This ensures configs like executor, environment, etc are maintained as it is from the first job
 first_job = jobs_to_exec[0]
 xformed_job = copy.deepcopy(processed_config['jobs'][first_job])
 
 # Add the merged steps to this newly introduced job
 xformed_job['steps'] = merged_steps
 
-# Create a duplicate config(transformed) with the newly introduced job (the only job in config)
+# Create a duplicate config(transformed) with the newly introduced job (as the only job in config)
 xformed_config = copy.deepcopy(processed_config)
 xformed_config['jobs'] = {}
 xformed_config['jobs'][xformed_job_name] = xformed_job
@@ -73,4 +75,9 @@ xformed_config['jobs'][xformed_job_name] = xformed_job
 with open(xformed_file, 'w+') as fstream:
     yaml.dump(xformed_config,fstream)
 
+# Locally execute the newly created job
+# This job has all the steps(ordered and merged) from the mentioned job along with it's ancestors
 subprocess.check_call('circleci local execute -c {} --job {}'.format(xformed_file, xformed_job_name), shell=True)
+
+# Clean up, remove the processed and transformed yml files
+subprocess.check_call('rm {} {}'.format(processed_file, xformed_file), shell=True)
